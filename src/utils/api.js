f3f09d453a968bb1ca16845647212c1036fe9627
haegurii <yoohaedong3@gmail.com>
2023-11-26 17:54:56 +0900

    dotenv

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,14 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 // const serverApi = "http://localhost:5002/api";
 // const serverApi = "http://172.16.104.219:5002/api";
 // const serverApi = "http://192.168.219.100:4000/api";
-const serverApi = "http://192.168.35.165:4000/api";
+
+const serverApi = API_KEY + ":4000/api";
+console.log(serverApi);
 
 export const getRoomExists = async (roomId) => {
   const response = await axios.get(`${serverApi}/room-exists/${roomId}`);
